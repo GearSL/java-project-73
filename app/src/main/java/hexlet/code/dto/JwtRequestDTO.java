3247a9473d7107a73e2de2f87eded44b0d782546
i.ismailov <ilyas.main@gmail.com>
2023-09-04 00:53:35 +0600

    Step #11: fixed test errors.

--- app/src/main/java/hexlet/code/dto/JwtRequestDTO.java
+++ app/src/main/java/hexlet/code/dto/JwtRequestDTO.java
@@ -2,9 +2,11 @@ package hexlet.code.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class JwtRequestDTO {
     private String email;
     private String password;
